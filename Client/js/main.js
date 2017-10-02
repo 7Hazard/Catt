83684ef9544323095b081870a0aa09735e08b628
Hazard <lolo9909@hotmail.com>
2017-10-02 14:01:01 +0200

    Init

---
+++ Client/js/main.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function(){
+    //UnhideAnimate();
+    OutputMessage("", "Catt", "Chat loaded");
+});
+
+function OutputMessage(timestamp, user, message){
+    document.getElementById("chatbox").innerHTML += 
+        "<div class='message white-text'><p>["+
+        timestamp
+        +"] </p><p>"+
+        user
+        +": </p><p>"+
+        message
+        +"</p></div>";
+}
---
+++ Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using SocketIo;
+using SocketIo.SocketTypes;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = Io.Create("127.0.0.1", 6969, 6969, SocketHandlerType.Udp);
+
+            socket.On("connect", () =>
+            {
+                
+            });
+        }
+    }
+}
